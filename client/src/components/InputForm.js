a33fcecbf9a262df9c1b93a072167c05bed68c63
phu <phu3365@gmail,com>
2023-10-09 12:44:21 +0700

    public

--- client/src/components/InputForm.js
+++ client/src/components/InputForm.js
@@ -1,11 +1,13 @@
 import React,{memo} from 'react'
 
-const InputForm = ({label}) => {
+const InputForm = ({label,name,password,type}) => {
   return (
     <div>
         <label htmlFor='phone' className='text-xs'>{label}</label>
-        <input type='text'
-        id='phone'
+        <input type={type}
+        id={name}
+        value={password}
+        required
         className='outline-none bg-[#e8f0fe] p-2 rounded-md w-full'/>
     </div>
   )
