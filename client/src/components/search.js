7150adada61030dd32ee8894073b2df66b886322
phu <phu3365@gmail,com>
2023-10-28 17:21:14 +0700

    fix css

--- client/src/components/search.js
+++ client/src/components/search.js
@@ -1,16 +1,15 @@
-import React,{memo}from 'react'
+import React, { memo } from 'react';
 
-const search = ({link,text,Icons}) => {
+const Search = ({ link, text, Icons }) => {
   return (
-    <div>
-        {/* <a href={link} className=" flex items-center p-3 text-center rounded block transition duration-300 hover:text-pink-500">
-            <span>{Icons && <Icons/>}</span>
-            <p>{text}</p>
-        </a> */}
-    <button onClick={link} className=" flex items-center p-3 text-center rounded block transition duration-300 hover:text-pink-500"><span>{Icons && <Icons/>}</span>
-            <p>{text}</p></button>
-    </div>
-  )
-}
+    <button
+      onClick={link}
+      className="flex p-1 items-center rounded block transition duration-300 hover:text-pink-500 hover:bg-gray-100 cursor-pointer"
+    >
+      <span>{Icons && <Icons />}</span>
+      <p className="ml-2">{text}</p>
+    </button>
+  );
+};
 
-export default memo(search)
+export default memo(Search);
