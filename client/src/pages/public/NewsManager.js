21db316d01e710dc07fa1654d4579731215556c5
phu <phu3365@gmail,com>
2023-10-29 04:49:40 +0700

    fix

--- client/src/pages/public/NewsManager.js
+++ client/src/pages/public/NewsManager.js
@@ -1,9 +1,4 @@
 import React, { useState, useEffect } from "react";
-import { getUserData,deletePost } from '../../api/api.js';
-import moment from 'moment';
-import Pagination from "./Pagination.js";
-import notfound from '../../assets/images/not_found.png'
-import EditPost from "./EditPost.js";
 import { deletePost, getUserData } from '../../api/api.js';
 import moment from 'moment';
 import Pagination from "./Pagination.js";
@@ -11,6 +6,7 @@ import notfound from '../../assets/images/not_found.png'
 import { toast } from 'react-toastify';
 import ConfirmationModal from '../../components/ConfirmationModal'
 import { Link } from "react-router-dom";
+import EditPost from "./EditPost.js";
 function TruncatedText({ text, maxLength }) {
   if (text.length <= maxLength) {
     return (
@@ -53,9 +49,9 @@ const NewsManager = () => {
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
   };
-  const handleDelete = async (id) => {
-      await deletePost(id);
-  }
+  // const handleDelete = async (id) => {
+  //     await deletePost(id);
+  // }
 
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
@@ -108,7 +104,6 @@ const NewsManager = () => {
 
   const [editingPost, setEditingPost] = useState(null);
     setSelectedImages(selectedImages.concat(selectedValidImages));
-  };
   const handleDelete = (postid) => {
         // Hiển thị toast để xác nhận trước khi xóa
         toast.info(
@@ -318,10 +313,6 @@ const hideToast = () => {
                                     className="h-5 w-5 text-red-700 cursor-pointer"
                                     viewBox="0 0 20 20"
                                     fill="currentColor"
-                                    onClick={() => {
-                                      const postIdToEdit = detailuser.posts.find((p) => p.id === post.id)?.id;
-                                      handleDelete(postIdToEdit);
-                                    }}
                                     onClick={() => handleDelete(post.id)}
                                   >
                                     <path
@@ -367,125 +358,7 @@ const hideToast = () => {
       )}
       {showForm && (
         <EditPost data={editingPost} toggleForm={toggleForm}/>
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-          <div className="mx-auto w-full max-w-[1000px] bg-white p-12 rounded-lg shadow-lg">
-            <form action="https://formbold.com/s/FORM_ID" method="PUT">
-              <div className="grid justify-items-end">
-
-              </div>
-              <div className="-mx-3 flex flex-rows">
-                <div className="w-full px-3 sm:w-1/2">
-                  <div className="">
-                    <label className=" block text-base font-medium text-[#07074D]">
-                      Tiêu Đề
-                    </label>
-                    <input
-                      type="text"
-                      value={editingPost.title}
-                      onChange={(e) => setEditingPost({ ...editingPost, title: e.target.value })}
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-                </div>
-                <div className="w-full px-3 ">
-                  <div className="">
-                    <label className=" block text-base font-medium text-[#07074D] ">
-                      Diện Tích
-                    </label>
-                    <input
-                      type="text"
-                      value={editingPost.area}
-                      onChange={(e) => setEditingPost({ ...editingPost, area: e.target.value })}
-                      className="w-full  rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className=" px-1">
-                <label className=" block text-base font-medium text-[#07074D]">
-                  Địa chỉ
-                </label>
-                <input
-                  type="text"
-                  value={editingPost.address}
-                  onChange={(e) => setEditingPost({ ...editingPost, address: e.target.value })}
-                  className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-              </div>
-              <div className="flex justify-between">
-                <div className="w-full">
-                  <div className="px-3 w-full ">
-                    <label className="block text-base font-medium text-[#07074D]">
-                      Mô Tả
-                    </label>
-                    <textarea
-                      value={editingPost.description}
-                      onChange={(e) => setEditingPost({ ...editingPost, description: e.target.value })}
-                      className="w-full h-40 resize-none appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-
-                  <div className="-mx-3 flex flex-col mb-4">
-                    <div className='flex flex-row'>
-                      <div className="w-full px-6 ">
-                        <div className="">
-                          <label className=" block text-base font-medium text-[#07074D]">
-                            Giá
-                          </label>
-                          <input
-                            type="text"
-                            value={editingPost.price}
-                            onChange={(e) => setEditingPost({ ...editingPost, price: e.target.value })}
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                          />
-                        </div>
-
-                      </div>
-
-                    </div>
-                    {/* <div className='flex flex-row'>
-                  <div className='w-full px-3 sw:w-1/2'>
-                    <label className=" block text-base font-medium text-[#07074D]">
-                      Giới tính
-                    </label>
-                    <select
-                      value={editingUser.gender}
-                      onChange={(e) => setEditingUser({ ...editingUser, gender: e.target.value === 'true' })}
-                      className="w-max-[50px] rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    >
-                      <option value={true}>Nam</option>
-                      <option value={false}>Nữ</option>
-                    </select>
-                  </div>
-
-
-                </div> */}
-
-
-                  </div>
-                </div>
-                
-                <div className="w-full sm:w-1/2 border border-black h-[200px]">
-                <label className=" block text-base font-medium text-[#07074D] ">
-                      Diện Tích
-                    </label>
-                </div>
-              </div>
-              <div className='flex gap-4'>
-                <button className="hover:shadow-form rounded-md bg-[#3eb15b] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-                // onClick={handleSave}
-                >
-                  Lưu
-                </button>
-                <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-                  onClick={toggleForm}
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+        
       )}
     </div>
   );
