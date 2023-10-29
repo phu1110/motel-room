d36f7d374d1f7409edc8df0eda9eabf350d9939d
Junnio <30598484+Junn1o@users.noreply.github.com>
2023-10-29 13:11:23 +0700

    he he

--- client/src/pages/public/NewsManager.js
+++ client/src/pages/public/NewsManager.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 import Pagination from "./Pagination.js";
 import notfound from '../../assets/images/not_found.png'
 import { toast } from 'react-toastify';
+import EditPost from "./EditPost.js";
 import ConfirmationModal from '../../components/ConfirmationModal'
 import { Link } from "react-router-dom";
 function TruncatedText({ text, maxLength }) {
@@ -59,8 +60,8 @@ const NewsManager = () => {
     }
   };
   const calculateElapsedTime = (post) => {
-    if (post && post.datecreatedroom) {
-      const postDate = moment(post.datecreatedroom);
+    if (post && post.dateCreated) {
+      const postDate = moment(post.dateCreated);
       const currentDate = moment();
 
       const hoursDiff = currentDate.diff(postDate, 'hours');
@@ -275,7 +276,7 @@ const hideToast = () => {
                                 {calculateElapsedTime(post)}
                               </td>
                               <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                19/09/2023
+                                {post.expireDate}
                               </td>
                               <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                 {post.status === 'Đang Chờ Duyệt' ? (
@@ -366,125 +367,7 @@ const hideToast = () => {
         <p>Đang lấy thông tin đợi chút nha </p>
       )}
       {showForm && (
-        <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-          <div className="mx-auto w-full max-w-[1000px] bg-white p-12 rounded-lg shadow-lg">
-            <form action="https://formbold.com/s/FORM_ID" method="PUT">
-              <div className="grid justify-items-end">
-
-              </div>
-              <div className="-mx-3 flex flex-rows">
-                <div className="w-full px-3 sm:w-1/2">
-                  <div className="">
-                    <label className=" block text-base font-medium text-[#07074D]">
-                      Tiêu Đề
-                    </label>
-                    <input
-                      type="text"
-                      value={editingPost.title}
-                      onChange={(e) => setEditingPost({ ...editingPost, title: e.target.value })}
-                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-                </div>
-                <div className="w-full px-3 ">
-                  <div className="">
-                    <label className=" block text-base font-medium text-[#07074D] ">
-                      Diện Tích
-                    </label>
-                    <input
-                      type="text"
-                      value={editingPost.area}
-                      onChange={(e) => setEditingPost({ ...editingPost, area: e.target.value })}
-                      className="w-full  rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-                </div>
-              </div>
-              <div className=" px-1">
-                <label className=" block text-base font-medium text-[#07074D]">
-                  Địa chỉ
-                </label>
-                <input
-                  type="text"
-                  value={editingPost.address}
-                  onChange={(e) => setEditingPost({ ...editingPost, address: e.target.value })}
-                  className="w-full appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                />
-              </div>
-              <div className="flex justify-between">
-                <div className="w-full">
-                  <div className="px-3 w-full ">
-                    <label className="block text-base font-medium text-[#07074D]">
-                      Mô Tả
-                    </label>
-                    <textarea
-                      value={editingPost.description}
-                      onChange={(e) => setEditingPost({ ...editingPost, description: e.target.value })}
-                      className="w-full h-40 resize-none appearance-none rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    />
-                  </div>
-
-                  <div className="-mx-3 flex flex-col mb-4">
-                    <div className='flex flex-row'>
-                      <div className="w-full px-6 ">
-                        <div className="">
-                          <label className=" block text-base font-medium text-[#07074D]">
-                            Giá
-                          </label>
-                          <input
-                            type="text"
-                            value={editingPost.price}
-                            onChange={(e) => setEditingPost({ ...editingPost, price: e.target.value })}
-                            className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                          />
-                        </div>
-
-                      </div>
-
-                    </div>
-                    {/* <div className='flex flex-row'>
-                  <div className='w-full px-3 sw:w-1/2'>
-                    <label className=" block text-base font-medium text-[#07074D]">
-                      Giới tính
-                    </label>
-                    <select
-                      value={editingUser.gender}
-                      onChange={(e) => setEditingUser({ ...editingUser, gender: e.target.value === 'true' })}
-                      className="w-max-[50px] rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
-                    >
-                      <option value={true}>Nam</option>
-                      <option value={false}>Nữ</option>
-                    </select>
-                  </div>
-
-
-                </div> */}
-
-
-                  </div>
-                </div>
-                
-                <div className="w-full sm:w-1/2 border border-black h-[200px]">
-                <label className=" block text-base font-medium text-[#07074D] ">
-                      Diện Tích
-                    </label>
-                </div>
-              </div>
-              <div className='flex gap-4'>
-                <button className="hover:shadow-form rounded-md bg-[#3eb15b] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-                // onClick={handleSave}
-                >
-                  Lưu
-                </button>
-                <button className="hover:shadow-form rounded-md bg-[#6A64F1] py-3 px-8 text-center text-base font-semibold text-white outline-none"
-                  onClick={toggleForm}
-                >
-                  Cancel
-                </button>
-              </div>
-            </form>
-          </div>
-        </div>
+         <EditPost data={editingPost} toggleForm={toggleForm}/>
       )}
     </div>
   );
